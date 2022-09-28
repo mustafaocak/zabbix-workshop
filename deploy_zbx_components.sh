554743915670fdd92bfa1928998d2036faf63d12
mustafaocak <mustafaocak@gmail.com>
2022-09-28 15:47:59 +0200

    renaming and exposing ports only locally

--- deploy_zbx_components.sh
+++ deploy_zbx_components.sh
@@ -1,4 +1,4 @@
-podman run --name mysql -v $(pwd)/my.cnf:/etc/my.cnf:Z --network podman  -p 3306:33060 -d docker.io/mysql/mysql-server:latest
+podman run --name mysql -v $(pwd)/my.cnf:/etc/my.cnf:Z --network podman  -p 127.0.0.1:3306:33060 -d docker.io/mysql/mysql-server:latest
 sleep 15
 source $(pwd)/env.sh
 
@@ -7,9 +7,9 @@ podman exec mysql mysql -u root -p$MYSQL_PASSWORD -e "CREATE USER 'zabbix'@'%' I
 podman exec mysql mysql -u root -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'zabbix'@'%' WITH GRANT OPTION"
 #podman exec mysql mysql -u root -p$MYSQL_PASSWORD -e "CREATE DATABASE zabbix"
 
-podman run --name zabbix-server --network podman  -p 10051:10051 -e DB_SERVER_HOST="mysql.dns.podman" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD=$MYSQL_PASSWORD -d docker.io/zabbix/zabbix-server-mysql:latest
+podman run --name zabbix-server --network podman  -p 127.0.0.1:10051:10051 -e DB_SERVER_HOST="mysql.dns.podman" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD=$MYSQL_PASSWORD -d docker.io/zabbix/zabbix-server-mysql:latest
 
-podman run --name zabbix-web -v $(pwd)/httpd.conf:/etc/apache2/httpd.conf:Z --network podman -e DB_SERVER_HOST="mysql.dns.podman" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD=$MYSQL_PASSWORD -e ZBX_SERVER_HOST="zabbix-server.dns.podman" -e PHP_TZ="Europe/Oslo" -p 8080:8080 -d docker.io/zabbix/zabbix-web-apache-mysql:latest
+podman run --name zabbix-web -v $(pwd)/httpd.conf:/etc/apache2/httpd.conf:Z --network podman -e DB_SERVER_HOST="mysql.dns.podman" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD=$MYSQL_PASSWORD -e ZBX_SERVER_HOST="zabbix-server.dns.podman" -e PHP_TZ="Europe/Oslo" -p 127.0.0.1:8080:8080 -d docker.io/zabbix/zabbix-web-apache-mysql:latest
 
 podman run --name zabbix-proxy --network podman -e DB_SERVER_HOST="mysql.dns.podman" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD=$MYSQL_PASSWORD -e ZBX_HOSTNAME=zabbix-proxy.dns.podman -e ZBX_SERVER_HOST="zabbix-server.dns.podman" -d docker.io/zabbix/zabbix-proxy-mysql
 
